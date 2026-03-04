12d6abfb2318e12e4448173427828552155c935a
Anna Ekmekci <anna.ekmekci@gmail.com>
2026-03-04 14:41:28 -0500

    env stuff

--- PlumbingProjectApp/backend/app.py
+++ PlumbingProjectApp/backend/app.py
@@ -21,9 +21,11 @@ from email_utils import generate_email_draft, generate_bulk_email_drafts
 app = Flask(__name__)
 CORS(app)
 
-cred = credentials.Certificate("serviceKey.json")
+service_key_json = os.environ.get("SERVICE_KEY")
+
+cred = credentials.Certificate(service_key_json)
 firebase_admin.initialize_app(cred)
-connection(from_file="serviceKey.json")
+connection(from_file=service_key_json)
 db = firestore.client()
 
 class Book(Model):
