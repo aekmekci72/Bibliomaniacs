6a0be4295b00ac5072243b813d3bd21cad4db7c2
bomiejun <116047701+bomiejun@users.noreply.github.com>
2026-04-07 11:32:30 -0400

    temp logs

--- PlumbingProjectApp/backend/app.py
+++ PlumbingProjectApp/backend/app.py
@@ -1033,8 +1033,11 @@ def get_email_draft_endpoint(review_id):
     if role != "admin":
         return jsonify({"error": "Permission denied"}), 403
     
+    print(review_id)
+    
     try:
         review = Review.collection.get(review_id)
+        print(review)
         
         # Determine status
         if review.date_processed is None:
