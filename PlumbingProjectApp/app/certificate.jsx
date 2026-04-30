a1f5a8acc592fa0ab767a83f6c7d46f8b148ad8c
bomiejun <116047701+bomiejun@users.noreply.github.com>
2026-04-30 11:41:21 -0400

    a

--- PlumbingProjectApp/app/certificate.jsx
+++ PlumbingProjectApp/app/certificate.jsx
@@ -402,6 +402,7 @@ export default function ArchiveScreen() {
                                     >
                                     <td className="px-8 py-2">{book.title}</td>
                                     <td className="px-4 py-2">{book.author}</td>
+                                    <td></td>
                                     </tr>
                                 ))
                                 ) : (
